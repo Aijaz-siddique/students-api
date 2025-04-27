4c26de0e6c9cba332cdc709fb625d9896ff6fc91
Aijaz-siddique <Aijazsiddique20@gmail.com>
2025-04-28 01:10:02 +0530

    Create storage.go

---
+++ internal/storage/storage.go
@@ -0,0 +1,9 @@
+package storage
+
+import "github.com/Aijaz-siddique/students-api/internal/types"
+
+type Storage interface {
+	CreateStudent(name string, email string, age int) (int64, error)
+	GetStudentById(id int64) (types.Student, error)
+	GetAllStudents() ([]types.Student, error)
+}
